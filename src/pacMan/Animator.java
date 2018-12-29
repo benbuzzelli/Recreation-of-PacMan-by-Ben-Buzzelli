af3cbdfaa6e75b719f0380dba87b423937da80db
benbuzzelli <benbuzz68@gmail.com>
2018-12-29 16:48:35 -0500

    Added comments

--- src/pacMan/Animator.java
+++ src/pacMan/Animator.java
@@ -1,7 +1,6 @@
 package pacMan;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
@@ -10,6 +9,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * 12/29/18
+ * 
+ * @author Ben Original animator class. Includes methods to generate animations
+ *         and sprite sheet animations by returning the appropriate image each
+ *         time the function is called.
+ */
 public class Animator {
 
 	private int frame = 0;
@@ -27,24 +33,25 @@ public class Animator {
 		return character;
 	}
 
-	public Image generateSpriteAnimation(int frame_gap, int num_sprites, String filename, PacManBoard pacman_board) throws IOException {
+	public Image generateSpriteAnimation(int frame_gap, int num_sprites, String filename, PacManBoard pacman_board)
+			throws IOException {
 		File imgFile = new File(filename);
 		BufferedImage spriteSheet = ImageIO.read(imgFile);
 		int sprite_width = spriteSheet.getWidth(pacman_board) / num_sprites;
 		int sprite_height = spriteSheet.getHeight(pacman_board);
-						
+
 		Image character;
 		Image[] sprites = new Image[num_sprites];
-		
+
 		for (int i = 0; i < num_sprites; i++) {
 			sprites[i] = spriteSheet.getSubimage(i * sprite_width, 0, sprite_width, sprite_height);
 		}
-		
+
 		character = spriteAnimatorHelper(frame_gap, sprites);
 
 		return character;
 	}
-	
+
 	public Image spriteAnimatorHelper(int frame_gap, Image[] sprites) {
 		int num_images = sprites.length;
 		int animation_length = num_images * frame_gap;
@@ -57,14 +64,5 @@ public class Animator {
 
 		return character;
 	}
-	
-	public static BufferedImage imageToBufferedImage(Image im) {
-	     BufferedImage bi = new BufferedImage
-	        (im.getWidth(null),im.getHeight(null),BufferedImage.TYPE_INT_RGB);
-	     Graphics bg = bi.getGraphics();
-	     bg.drawImage(im, 0, 0, null);
-	     bg.dispose();
-	     return bi;
-	  }
 
 }
